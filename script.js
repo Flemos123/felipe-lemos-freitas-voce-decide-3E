66c16489b797518373e71da84c5837cc696aa471
Flemos123 <164080857+Flemos123@users.noreply.github.com>
2024-05-29 23:47:41 +0000

    29/05/24

---
+++ script.js
@@ -0,0 +1,47 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultados = document.querySelector(".caixa-resultados");
+const textoResultado = document.querySelector(".texto-resutado");
+
+const perguntas = [
+  {
+    enunciado: "Pergunta 1",
+    alternativas: [
+      "Respota 1",
+      "Respota 2"
+    ]
+  },
+
+  {
+    enunciado: "Pergunta 2",
+    alternativas: [
+      "Respota 1",
+      "Respota 2"
+    ]
+  },
+
+  {
+    enunciado: "Pergunta 3",
+    alternativas: [
+      "Respota 1",
+      "Respota 2"
+    ]
+  },
+
+  {
+    enunciado: "Pergunta 4",
+    alternativas: [
+      "Respota 1",
+      "Respota 2"
+    ]
+  },
+
+  {
+    enunciado: "Pergunta 5",
+    alternativas: [
+      "Respota 1",
+      "Respota 2"
+    ]
+  }
+];
